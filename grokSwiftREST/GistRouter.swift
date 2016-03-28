5d65fe3491cb3d4827a2f10c064d543a1d7eaf83
Adrian Bolinger <adrian.bolinger@me.com>
2016-03-28 07:12:42 -0400

    Initial Commit

---
+++ grokSwiftREST/GistRouter.swift
@@ -0,0 +1,59 @@
+//
+//  GistRouter.swift
+//  grokSwiftREST
+//
+//  Created by Christina Moulton on 2015-11-29.
+//  Copyright © 2015 Teak Mobile Inc. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+enum GistRouter: URLRequestConvertible {
+  static let baseURLString:String = "https://api.github.com"
+  
+  case GetPublic() // GET https://api.github.com/gists/public
+  case GetMyStarred() // GET https://api.github.com/gists/starred
+  case GetAtPath(String) // GET at given path
+  
+  var URLRequest: NSMutableURLRequest {
+    var method: Alamofire.Method {
+      switch self {
+      case .GetPublic:
+        return .GET
+      case .GetMyStarred:
+        return .GET
+      case .GetAtPath:
+        return .GET
+      }
+    }
+    
+    let result: (path: String, parameters: [String: AnyObject]?) = {
+      switch self {
+      case .GetPublic:
+        return ("/gists/public", nil)
+      case .GetMyStarred:
+        return ("/gists/starred", nil)
+      case .GetAtPath(let path):
+        let URL = NSURL(string: path)
+        let relativePath = URL!.relativePath!
+        return (relativePath, nil)
+      }
+    }()
+    
+    let URL = NSURL(string: GistRouter.baseURLString)!
+    let URLRequest = NSMutableURLRequest(URL: URL.URLByAppendingPathComponent(result.path))
+    
+    // Set OAuth token if we have one
+    if let token = GitHubAPIManager.sharedInstance.OAuthToken {
+      URLRequest.setValue("token \(token)", forHTTPHeaderField: "Authorization")
+    }
+    
+    let encoding = Alamofire.ParameterEncoding.JSON
+    let (encodedRequest, _) = encoding.encode(URLRequest, parameters: result.parameters)
+    
+    encodedRequest.HTTPMethod = method.rawValue
+    
+    return encodedRequest
+  }
+}
